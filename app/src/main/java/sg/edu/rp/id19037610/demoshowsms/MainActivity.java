03770d0ffa2be2ded8db2bade6a01c8c3137946a
19037610 <19037610@myrp.edu.sg>
2021-05-25 11:43:00 +0800

    completed

--- app/src/main/java/sg/edu/rp/id19037610/demoshowsms/MainActivity.java
+++ app/src/main/java/sg/edu/rp/id19037610/demoshowsms/MainActivity.java
@@ -1,14 +1,104 @@
 package sg.edu.rp.id19037610.demoshowsms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvSMS;
+    Button btnRetrieve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSMS = findViewById(R.id.tv);
+        btnRetrieve = findViewById(R.id.btnRetrieve);
+
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int permissionCheck = PermissionChecker.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS);
+
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, 0);
+                    // stops the action from proceeding further as permission
+                    // not granted yet
+                    return;
+                }
+
+                Uri uri = Uri.parse("content://sms");
+
+                // The columns we want
+                // date is when the message took place
+                // address is the number of the other party
+                // body is the message content
+                // type 1 is received, type 2 is sent
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+
+                String filter = "body LIKE ? AND body LIKE ?";
+
+                String[] filterArgs = {"%late%", "%min%"};
+
+                // Get Content Resolver object from which to
+                // query the content provider
+                ContentResolver cr = getContentResolver();
+                // Fetch SMS Message from Built-in Content Provider
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date
+                                + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                }
+                tvSMS.setText(smsBody);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 0: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    btnRetrieve.performClick();
+                } else {
+                    // permission denied
+                    Toast.makeText(MainActivity.this, "Permission not granted",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
